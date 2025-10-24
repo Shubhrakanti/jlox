e092b8baa2415e121d2c0bf9635ad1a50a11cb2a
Shubhra <shubhrakanti@berkeley.edu>
2025-10-24 16:20:48 -0700

    add assignments

--- app/src/main/java/lox/Interpreter.java
+++ app/src/main/java/lox/Interpreter.java
@@ -38,6 +38,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
+
+    @Override
     public Void visitVarStmt(Var stmt) {
         Object value = null;
         if (stmt.initializer != null) {
