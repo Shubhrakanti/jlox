668c99922634b03aec3073196337d188175c4041
Shubhra <shubhrakanti@berkeley.edu>
2025-11-07 16:31:19 -0800

    function calling

--- app/src/main/java/lox/Interpreter.java
+++ app/src/main/java/lox/Interpreter.java
@@ -1,8 +1,9 @@
 package lox;
 
-import java.nio.channels.Pipe.SourceChannel;
+import java.util.ArrayList;
 import java.util.List;
 
+import lox.Expr.Call;
 import lox.Expr.Variable;
 import lox.Stmt.Expression;
 import lox.Stmt.If;
@@ -10,7 +11,22 @@ import lox.Stmt.Print;
 import lox.Stmt.Var;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-    private Environment environment = new Environment();
+    private Environment gloabls = new Environment();
+    private Environment environment = gloabls;
+
+    Interpreter() {
+        gloabls.define("clock", new LoxCallable() {
+            @Override
+            public int arity() {
+                return 0;
+            }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double) System.currentTimeMillis() / 1000.0;
+            }
+        });
+    }
 
     void interpret(List<Stmt> statements) {
         try {
@@ -229,5 +245,29 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    @Override
+    public Object visitCallExpr(Call expr) {
+        Object callee = evaluate(expr.callee);
+
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) {
+            arguments.add(evaluate(argument));
+        }
+
+        if (!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren,
+                    "Can only call functions and classes.");
+        }
+
+        LoxCallable function = (LoxCallable) callee;
+        if (arguments.size() != function.arity()) {
+            throw new RuntimeError(expr.paren, "Expected " +
+                    function.arity() + " arguments but got " +
+                    arguments.size() + ".");
+        }
+
+        return function.call(this, arguments);
     }
 }
